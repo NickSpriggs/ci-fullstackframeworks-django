e3757cfe36d468c7505fcab4c7ac4ddc979eec72
NickSpriggs <ntspriggs@me.com>
2021-11-02 00:40:50 +0000

    Edited everything up to and including the modifying section of Django

--- todo/forms.py
+++ todo/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from .models import Item
 
 class ItemForm(forms.ModelForm):
+    #inherits all the functionality of forms.ModelForm
     class Meta:
+        #Meta gives form information about itself such as display
         model = Item
         fields = ['name', 'done']
