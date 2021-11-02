cd411be2c99867fec27775a724fe87439b6f8979
NickSpriggs <ntspriggs@me.com>
2021-11-02 17:24:16 +0000

    Up to and including the testing pages. However I did skip over the last video and my own code for the testing section seemd a little faulty

--- todo/models.py
+++ todo/models.py
@@ -4,9 +4,10 @@ from django.db import models
 
 
 class Item(models.Model):
+    # Item(models.Model) means it inherits from the Model class
     name = models.CharField(max_length=50, null=False, blank=False)
     done = models.BooleanField(null=False, blank=False, default=False)
 
-    # Overrides the mdoel string method to show item's name instead
+    # Overrides the model string method to show item's name instead
     def __str__(self):
         return self.name
